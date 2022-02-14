1ce8d2dc8e77b05b4773d3c45bbef5bbd7f65ac3
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-02-14 17:37:28 -0500

    Taller 1

---
+++ Taller 1_00020316700/contacto/formulario.js
@@ -0,0 +1,13 @@
+function informacion()
+{    
+    //debugger;
+
+    var nombre = document.getElementById("superNombre");
+    var Telefono = document.getElementById("telefono");
+    var correo = document.getElementById("correo");
+
+    console.log("NOMBRE",nombre);
+    
+    alert("Nombre:" + nombre + "\nTelefono: " + Telefono + "\nCorreo:" + correo);
+
+}
